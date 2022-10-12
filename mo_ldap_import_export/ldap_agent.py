aebf2c42e3d1fd0969d7fb7581828a3ed93c6778
Lars Peter Thomsen <larsp@magenta.dk>
2022-10-12 14:20:34 +0200

    More setup

--- mo_ldap_import_export/ldap_agent.py
+++ mo_ldap_import_export/ldap_agent.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Magenta ApS
+#
+# SPDX-License-Identifier: MPL-2.0
+
 # Opret en agent der modtager amqp-events og importerer og eksporterer til LDAP
 from mo_ldap_import_export.config import Settings
 
